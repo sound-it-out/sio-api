1ef7491f2e9252b43d8ac6ec2099773ff575027e
Matt Hope <matthew.hope396@outlook.com>
2019-09-10 13:58:35 +0100

    WIP: AWS polly implementation.

---
+++ src/SIO.Infrastructure.AWS/Speech/AWSSpeechClient.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.Polly;
+using Amazon.Polly.Model;
+using Amazon.Runtime;
+using SIO.Infrastructure.AWS.File;
+using SIO.Infrastructure.File;
+using SIO.Infrastructure.Speech;
+
+namespace SIO.Infrastructure.AWS.Speech
+{
+    internal class AWSSpeechClient : ISpeechClient<AWSSpeechRequest>
+    {
+        public readonly IAmazonPolly _pollyClient;
+        private readonly IFileClient _fileClient;
+
+        public AWSSpeechClient()
+        {
+            _pollyClient = new AmazonPollyClient(new BasicAWSCredentials("", ""), RegionEndpoint.EUWest1);
+            _fileClient = new S3FileClient();
+        }
+        public async ValueTask<ISpeechResult> TranslateTextAsync(AWSSpeechRequest request)
+        {         
+            var synthesizeSpeechRequest = new StartSpeechSynthesisTaskRequest
+            {
+                OutputFormat = request.OutputFormat,
+                VoiceId = request.VoiceId,
+                Text = request.Text
+            };
+
+            var response = await _pollyClient.StartSpeechSynthesisTaskAsync(synthesizeSpeechRequest);            
+
+            using (var waitHandle = new AutoResetEvent(false))
+            using (var timer = new Timer(
+                    callback: async (e) => { await CheckSynthesisTask(response.SynthesisTask.TaskId, waitHandle); },
+                    state: null,
+                    dueTime: TimeSpan.Zero,
+                    period: TimeSpan.FromSeconds(30)
+                )
+            )
+            {
+                waitHandle.WaitOne();
+            }
+
+            var task = await _pollyClient.GetSpeechSynthesisTaskAsync(new GetSpeechSynthesisTaskRequest { TaskId = response.SynthesisTask.TaskId });
+
+            var fileResult = await _fileClient.DownloadAsync(task.SynthesisTask.OutputUri, "");
+            
+            return new AWSSpeechResult(fileResult.OpenStreamAsync);
+        }
+
+        private async Task CheckSynthesisTask(string id, EventWaitHandle waitHandle)
+        {
+            var task = await _pollyClient.GetSpeechSynthesisTaskAsync(new GetSpeechSynthesisTaskRequest { TaskId = id });
+
+            if (task.SynthesisTask.TaskStatus == Amazon.Polly.TaskStatus.Completed)
+                waitHandle.Set();
+        }
+    }
+}
---
+++ src/SIO.Infrastructure.AWS/Speech/AWSSpeechResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using SIO.Infrastructure.Speech;
+
+namespace SIO.Infrastructure.AWS.Speech
+{
+    public class AWSSpeechResult : ISpeechResult
+    {
+        private readonly Func<ValueTask<Stream>> _stream;
+
+        public AWSSpeechResult(Func<ValueTask<Stream>> func)
+        {
+            _stream = func;
+        }
+        public ValueTask<Stream> OpenStreamAsync()
+        {
+            return _stream.Invoke();
+        }
+    }
+}
